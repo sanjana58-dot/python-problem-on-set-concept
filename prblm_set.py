b9860f32a8d56384d42fdb0efe710ca27476dd39
sanjana58-dot <155241755+sanjana58-dot@users.noreply.github.com>
2024-06-15 15:19:56 +0530

    Add files via upload

---
+++ prblm_set.py
@@ -0,0 +1,9 @@
+#you are given a list of subjects for students . Assume one classroom is required for 1 subject . How many classrooms are required by all students
+
+#"python", "java","C++","python","javascript","java","pyhton","java","C++","C"
+
+
+L = ["python", "java","C++","python","javascript","java","pyhton","java","C++","C"]  #given list
+subjects = {"python", "java","C++","python","javascript","java","pyhton","java","C++","C"}  #copy elemnets to dictionary coz ictionary ignores repetition
+no_stu = len(subjects)  #calculated length of dictionary
+print("Number of classrooms needed by all students :",no_stu)
