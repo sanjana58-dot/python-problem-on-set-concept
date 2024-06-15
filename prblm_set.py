d4ce769f5b2e9e91594f1d717b3300adfb86906b
sanjana58-dot <155241755+sanjana58-dot@users.noreply.github.com>
2024-06-15 15:22:48 +0530

    Update prblm_set.py

--- prblm_set.py
+++ prblm_set.py
@@ -4,6 +4,6 @@
 
 
 L = ["python", "java","C++","python","javascript","java","pyhton","java","C++","C"]  #given list
-subjects = {"python", "java","C++","python","javascript","java","pyhton","java","C++","C"}  #copy elemnets to dictionary coz ictionary ignores repetition
+subjects = {"python", "java","C++","python","javascript","java","pyhton","java","C++","C"}  #copy elemnets to dictionary coz dictionary ignores repetition
 no_stu = len(subjects)  #calculated length of dictionary
 print("Number of classrooms needed by all students :",no_stu)
